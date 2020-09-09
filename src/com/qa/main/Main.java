f245426a00d010eed53418452b7715bf0f673fc3
Mohammad Rashid <MRashid1@qa.com>
2020-09-09 09:40:33 +0100

    Initial Commit

---
+++ src/com/qa/main/Main.java
@@ -0,0 +1,18 @@
+package com.qa.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Operations opp = new Operations();
+
+//		System.out.println(opp.add(10, 5));
+//		System.out.println(opp.substr(15, 12));
+//		System.out.println(opp.mult(5, 7));
+		System.out.println(opp.div(7, 15));
+		System.out.println(opp.div(14, 2));
+//		System.out.println(opp.mod(1024, 100));
+//		System.out.println(opp.indices(2, 2));
+	}
+
+}
---
+++ src/com/qa/main/Operations.java
@@ -0,0 +1,39 @@
+package com.qa.main;
+
+public class Operations {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int substr(int a, int b) {
+		return a - b;
+	}
+
+	public int mult(int a, int b) {
+		return a * b;
+	}
+
+	public int mod(int a, int b) {
+		return a % b;
+	}
+
+	public int div(int a, int b) {
+		try {
+
+			if (a < b) {
+				throw new NumberFormatTooBigException();
+			} else {
+				return a / b;
+			}
+		} catch (NumberFormatTooBigException nftbe) {
+			System.out.println("Catching exception from nftbe class");
+			nftbe.printStackTrace();
+			return 0;
+		}
+	}
+
+	public int indices(int a, int b) {
+		return (int) Math.pow(a, b);
+	}
+}
